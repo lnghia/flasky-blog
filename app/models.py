e183baf52dcdaeed29668e3ad306ccd3c2d7c632
n3wus3r <trongnghialedinh@gmail.com>
2020-08-30 18:48:43 +0700

    add ondelete=CASCADE to foreign key

--- app/models.py
+++ app/models.py
@@ -54,7 +54,7 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(64), unique=True, index=True)
     email = db.Column(db.String(64), unique=True, index=True)
     password_hash = db.Column(db.String(128))
-    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id', ondelete="CASCADE"))
     confirmed = db.Column(db.Boolean, default=False)
     member_since = db.Column(db.DateTime, default=datetime.utcnow)
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
